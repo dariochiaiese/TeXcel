6239d6af0defbd6b2649aaea7b6b8625a0303533
Dario Chiaiese <37512025+dariochiaiese@users.noreply.github.com>
2022-04-01 18:13:16 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='texcel',
+      version='3.1.3',
+      description='This program reads Excel tables from .xlsx (or .xls) files and outputs them in LaTeX fromat',
+      url='http://github.com/dariochiaiese/texcel',
+      author='Dario Chiaiese',
+      author_email='dariochiaiese@gmail.com',
+      license='GPLv3',
+      packages=['texcel'],
+      install_requires=[
+          'Tk==0.1.0',
+          'pandas',
+          'openpyxl',
+      ],
+      include_package_data=True,
+      package_data={'': ['*.txt']},
+      zip_safe=False)
